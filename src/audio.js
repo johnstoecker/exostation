d81404d34c09a41ba4c5e7b0d73edbb28bfa6545
John Stoecker <johnstoecker@gmail.com>
2020-07-03 15:44:15 -0700

    chill cafe audio

--- src/audio.js
+++ src/audio.js
@@ -1,4 +1,4 @@
-var sunsetAudio = {
+var exostationAudio = {
   paused: false,
   audio: undefined,
   audioContext: undefined,
@@ -8,54 +8,46 @@ var sunsetAudio = {
 };
 
 
-// // audio.src = 'http://192.99.37.181:8050/live.mp3';
-// poolside.fm
-// audio.src = 'http://stream.radio.co/s98f81d47e/listen';
-// outrun
-// http://139.162.14.151:9090/160mp3/;?d=
-// synthwave
-// http://139.162.14.151:9090/160mp3/;?d
-// synthwave - KJR the drop http://www.kickinjamzradio.com
-// http://192.227.116.104:8367/;stream/1
+//http://hirschmilch.de:7000/listen.pls?sid=3&t=.m3u
+//http://95.217.39.141:9125/listen.pls?sid=1&t=.m3u
+//http://192.99.17.12:4516/listen.pls?sid=1&t=.m3u
+//http://149.56.157.81:5104/listen.pls?sid=1&t=.m3u jrfmnetwork.com JR.FM Chill/Lounge Radio
 
-// http://79.120.39.202:8000/retrowave
-// radio caprice space synth -- http://radcap.ru
-// http://79.120.39.202:8000/space synth
 
-// Radio Record Synthwave -- https://www.radiorecord.ru/
-// http://air.radiorecord.ru:805/synth_128
+
+//https://www.internet-radio.com/servers/tools/playlistgenerator/?u=http://192.99.35.215:5078/listen.pls?sid=1&t=.m3u
+// Chill Cafe
+// Rainy Days In Tokyo Lofi Hip Hop Jazzhop Chillhop Mix - Beats to chillstudyrelax [2cSj]
+// https://a1airadionetwork.com
+
 
 var streams = [
-  { name: "poolside.fm", src: "http://stream.radio.co/s98f81d47e/listen", songURL: "https://public.radio.co/stations/s98f81d47e/status"},
-  { name: "Radio Record Synthwave", src: "http://air.radiorecord.ru:805/synth_128", details: "Radio Record Synthwave -- https://www.radiorecord.ru/"},
-  { name: "Radionomy Retrowave", src: "http://139.162.14.151:9090/160mp3/;?d", details: "Synthwave Retrowave Radionomy", songURL: "http://139.162.14.151:9090/7.html"},
-  { name: "Radio Caprice Retrowave", src: "http://79.120.39.202:8000/retrowave", details: "Radio Caprice http://radcap.ru"},
-  { name: "Radio Caprice Space Synth", src: "http://79.120.39.202:8000/spacesynth", details: "Radio Caprice http://radcap.ru"}
+  { name: "Chill Cafe", src: "http://192.99.35.215:5078/IRN_Rocks", details: "Chill Cafe https://a1airadionetwork.com" }
 ];
 
 var currentStreamIndex = 0;
 
-function nextStream() {
-  currentStreamIndex = (currentStreamIndex + 1) % streams.length;
-  if (!sunsetAudio.paused) {
-    playPause();
-  }
-  if (sunsetAudio.audio) {
-    stopStream();
-  }
-  playPause();
-}
+// function nextStream() {
+//   currentStreamIndex = (currentStreamIndex + 1) % streams.length;
+//   if (!exostationAudio.paused) {
+//     playPause();
+//   }
+//   if (exostationAudio.audio) {
+//     stopStream();
+//   }
+//   playPause();
+// }
 
 function playPause() {
   if (document.getElementById("radioStream")) {
-    if (sunsetAudio.paused) {
+    if (exostationAudio.paused) {
       document.getElementById("radioStream").play();
-      sunsetAudio.paused = false;
+      exostationAudio.paused = false;
       document.getElementById("playButton").style.display = 'none';
       document.getElementById("pauseButton").style.display = 'block';
     } else {
       document.getElementById("radioStream").pause();
-      sunsetAudio.paused = true;
+      exostationAudio.paused = true;
       document.getElementById("playButton").style.display = 'block';
       document.getElementById("pauseButton").style.display = 'none';
 
@@ -68,93 +60,37 @@ function playPause() {
   }
 }
 
-// TODO
 function stopStream() {
    var elem = document.getElementById("radioStream");
    return elem.parentNode.removeChild(elem);
-   sunsetAudio.scriptProcessorNode.disconnect(sunsetAudio.audioContext.destination);
-   delete sunsetAudio.audio;
-   delete sunsetAudio.audioContext;
-   delete sunsetAudio.mediaElementSource;
-   delete sunsetAudio.analyzer;
-   delete sunsetAudio.analyzerBytes;
-   delete sunsetAudio.scriptProcessorNode;
+   exostationAudio.scriptProcessorNode.disconnect(exostationAudio.audioContext.destination);
+   delete exostationAudio.audio;
+   delete exostationAudio.audioContext;
+   delete exostationAudio.mediaElementSource;
+   delete exostationAudio.analyzer;
+   delete exostationAudio.analyzerBytes;
+   delete exostationAudio.scriptProcessorNode;
 }
 
 function startStream() {
   // // Create a new instance of an audio object and adjust some of its properties
-  sunsetAudio.audio = new Audio();
+  let chillAudio = new Audio();
 
-  // TODO: get metatdata loading for song titles
-  sunsetAudio.onloadedmetadata = function() {
-      console.log("Meta data for audio loaded");
-  };
-
-  sunsetAudio.audio.src = streams[currentStreamIndex].src;
+  chillAudio.src = streams[currentStreamIndex].src;
   console.log("loading " + streams[currentStreamIndex].details);
   document.getElementById("currentStation").innerHTML = streams[currentStreamIndex].name;
-  sunsetAudio.audio.crossOrigin = "anonymous";
-  sunsetAudio.audio.controls = false;
-  sunsetAudio.audio.style.position = "absolute"
-  sunsetAudio.audio.style.bottom = "10px"
-  sunsetAudio.audio.id = "radioStream"
-  document.getElementById('audio_box').appendChild(sunsetAudio.audio);
+  // exostationAudio.audio.crossOrigin = "anonymous";
+  // exostationAudio.audio.controls = false;
+  // exostationAudio.audio.style.position = "absolute"
+  // exostationAudio.audio.style.bottom = "10px"
+  // exostationAudio.audio.id = "radioStream"
+  // document.getElementById('audio_box').appendChild(exostationAudio.audio);
 
-  window.AudioContext = window.AudioContext || window.webkitAudioContext;
-  sunsetAudio.audioContext = new AudioContext();
-  sunsetAudio.mediaElementSource = sunsetAudio.audioContext.createMediaElementSource(sunsetAudio.audio);
-  sunsetAudio.analyzer = sunsetAudio.audioContext.createAnalyser();
-  sunsetAudio.analyzer.smoothingTimeConstant = 0.1;
-  sunsetAudio.analyzer.fftSize = 1024;
-
-  sunsetAudio.mediaElementSource.connect(sunsetAudio.analyzer);
-  sunsetAudio.analyzer.connect(sunsetAudio.audioContext.destination);
-  sunsetAudio.analyzerBytes = new Uint8Array(sunsetAudio.analyzer.frequencyBinCount);
-  sunsetAudio.audio.play();
-
-
-
-
-  // Create new instance of AudioContext
-  // var audioContext = new AudioContext();
-  // Set the source with the HTML Audio Node
-  // var source = audioContext.createMediaElementSource(document.getElementById('track'));
-  // Set the scriptProcessorNode to get PCM data in real time
-  sunsetAudio.scriptProcessorNode = sunsetAudio.audioContext.createScriptProcessor(4096, 1, 1);
-  // Connect everythings together
-  sunsetAudio.scriptProcessorNode.connect(sunsetAudio.audioContext.destination);
-  sunsetAudio.mediaElementSource.connect(sunsetAudio.scriptProcessorNode);
-  sunsetAudio.mediaElementSource.connect(sunsetAudio.audioContext.destination);
-
-  var onAudioProcess = new RealTimeBPMAnalyzer({
-      scriptNode: {
-          bufferSize: 4096,
-          numberOfInputChannels: 1,
-          numberOfOutputChannels: 1
-      },
-      pushTime: 10000,
-      pushCallback: function (err, bpm) {
-          console.log('bpm', bpm);
-          // counts over 100 are unreliable
-          if (bpm && bpm[0].count < 100) {
-            var currentBPM = bpm[0].tempo;
-            window.currentBeatDelta = 60.0/currentBPM;
-          }
-      }
-  });
-  // Attach realTime function to audioprocess event.inputBuffer (AudioBuffer)
-  sunsetAudio.scriptProcessorNode.onaudioprocess = function (e) {
-      onAudioProcess.analyze(e);
-  };
-
-
-
-  window.sunsetAnalyzer = sunsetAudio.analyzer
-  console.log(sunsetAudio.audioContext.sampleRate);
+  exostationAudio.audio = chillAudio;
+  chillAudio.play();
 }
 
 export default {
-  sunsetAudio: sunsetAudio,
-  playPause: playPause,
-  nextStream: nextStream
+  exostationAudio: exostationAudio,
+  playPause: playPause
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import world from './world';
+import audio from './audio';
 
 window.onload = function() {
   world.createWorld();
+  // audio.playPause();
+  document.getElementById("playButton").addEventListener("click", audio.playPause);
+  document.getElementById("pauseButton").addEventListener("click", audio.playPause);
 }
